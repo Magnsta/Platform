6d3839f2b0126edf7a43daef67b36349835c948f
Magnus <magnsta@stud.ntnu.no>
2020-11-16 20:47:37 +0100

    Update MotorWriter.java

--- java/MotorWriter.java
+++ java/MotorWriter.java
@@ -2,7 +2,9 @@
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-
+import com.pi4j.io.*;
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.GpioUtil;
 public class MotorWriter implements Runnable {
   private final long FREQ;
   private final String PORT;
@@ -13,7 +15,17 @@ public class MotorWriter implements Runnable {
   private int maxEnc; //maximum encoder value
   private int minEnc; // minimum encoder value
 
+  final GpioController gpio = GpioFactory.getInstance();
+
+  Gpio.pinMode(0,Gpio.INPUT); //11 Sets pins on Raspberry pi as input
+  Gpio.pinMode(1,Gpio.INPUT); //12
+  Gpio.pinMode(2,Gpio.INPUT); //13
+  Gpio.pinMode(3,Gpio.INPUT); //15
+  Gpio.pinMode(4,Gpio.INPUT); //16
+  Gpio.pinMode(5,Gpio.INPUT); //18
+
   public MotorWriter(long freq, StorageBox box, String address) {
+
     this.PORT = address;
     startSerialCom();
     this.mc1 = new RoboClaw(0x80);
@@ -113,38 +125,44 @@ public class MotorWriter implements Runnable {
 
   }
 
-
-  private void resetMotorsAndEncoders() {/*
-    driveM1(-100);
+  /**
+   * ////TODO save MaxEnc and MinEnc values
+   * ////TODO Make method to find MinEnc aswell
+   */
+  private void resetMotorsAndEncoders() {
+    //boolean atStart = false;
     boolean atEnd = false;
     while (!atEnd) {
+      driveM1(-100);
       throw new RuntimeException("program the reset motors method you dufus!");
       //getInput form sensor
-      atEnd = getInputFormSensor(1);
+      atEnd = getInputFormSensor(0);
     }
     sendCommandArray(mc1.stopM1());
+    setMotorPos(1, (int) ((maxEnc - minEnc) / 2));
     sendCommandArray(mc1.resetEnc1Cmd());
 
-    driveM2(-100);
     atEnd = false;
     while (!atEnd) {
+      driveM2(-100);
       //getInput form sensor
       atEnd = getInputFormSensor(2);
     }
+
     sendCommandArray(mc1.stopM2());
+    setMotorPos(2, (int) ((maxEnc - minEnc) / 2));
+
     sendCommandArray(mc1.resetEnc2Cmd());
-    driveM3(-100);
+
     atEnd = false;
     while (!atEnd) {
-      //getInput form sensor
-      atEnd = getInputFormSensor(3);
+      driveM3(-100);
+      //getInput from sensor
+      atEnd = getInputFormSensor(4);
     }
     sendCommandArray(mc2.stopM1());
+    setMotorPos(3, (int) ((maxEnc - minEnc) / 2));
     sendCommandArray(mc2.resetEnc1Cmd());
-
-    setMotorPos(1, (int) ((maxEnc - minEnc) / 2));
-    setMotorPos(2, (int) ((maxEnc - minEnc) / 2));
-    setMotorPos(3, (int) ((maxEnc - minEnc) / 2));*/
   }
 
   private void sendCommand(byte[] cmd) {
@@ -181,6 +199,10 @@ public class MotorWriter implements Runnable {
     return (int) Math.round(projectDouble(motorAngle, maxEnc, minEnc, MotorController.getOperatingAngle()));
   }
 
+  /**
+   * @param motorAngels
+   * @return
+   */
   private int[] getEncPositions(double[] motorAngels) {
     int[] returnInts = new int[motorAngels.length];
     for (int i = 0; i < motorAngels.length; i++) {
@@ -189,6 +211,74 @@ public class MotorWriter implements Runnable {
     return returnInts;
   }
 
+  /**
+   * @param sensor
+   * @return
+   */
+  private boolean getInputFormSensor(int sensor){
+    if(sensor == 1) {
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    if(sensor == 2){
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    if(sensor == 3){
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    if(sensor == 4){
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    I
+    if(sensor == 5){
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    I
+    if(sensor == 6){
+      int value = Gpio.digitalRead(sensor)
+      if(value != 1)
+      {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+  }
   private double projectDouble(double value, int max, int min, double scale) {
     return (value * ((max - min) / scale));
   }
@@ -204,4 +294,6 @@ public class MotorWriter implements Runnable {
   private void driveM3(int speed) {
     sendCommandArray(mc2.driveM1Cmd(speed));
   }
+
+
 }
